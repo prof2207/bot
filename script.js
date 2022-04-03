d62c6c78e862edb0d681bd18965f73fd695785dc
Roman Punko <prof2207@mail.ru>
2022-04-03 23:59:51 +0300

    lesson06

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+
+function isNum(number) {
+    return !isNaN(parseFloat(number)) && isFinite(number);
+}
+function startGame(randomNumber) {
+    randomNumber = Math.floor(Math.random() * 100);
+    console.log(randomNumber);
+    return function repeat() {
+        let userNumber = prompt('Число?');
+        if (isNum(userNumber)) {
+            if (userNumber > randomNumber) {
+                alert("Загаданное число меньше");
+            } else if (userNumber < randomNumber) {
+                alert("Загаданное число больше");
+            } else {
+                alert("Поздравляю, Вы угадали!!!");
+            }
+        } else {
+            alert("Введи число!");
+        }
+    };
+
+}
+startGame()();
+
+
+
+
